2bc020c69aa2a996eb7844accf3e8b93d5ba8653
keshav.jha <keshav.jha@medibuddy.in>
2022-09-18 19:42:17 +0530

    Added ErrorValidationTest

---
+++ src/test/java/academy/tests/ErrorValidationsTest.java
@@ -0,0 +1,43 @@
+package academy.tests;
+
+import org.testng.annotations.Test;
+
+import java.io.IOException;
+import java.util.List;
+
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
+
+import academy.testcomponents.BaseTest;
+import academy.testcomponents.Retry;
+import academy.pageobjects.CartPage;
+import academy.pageobjects.ProductCatalogue;
+
+public class ErrorValidationsTest extends BaseTest {
+
+    @Test(groups = {"ErrorHandling"}, retryAnalyzer = Retry.class)
+    public void LoginErrorValidation() throws IOException, InterruptedException {
+
+
+        landingPage.loginApplication("anshika@gmail.com", "Iamki000");
+        Assert.assertEquals("Incorrect email or password.", landingPage.getErrorMessage());
+
+    }
+
+
+    @Test
+    public void ProductErrorValidation() throws IOException, InterruptedException {
+
+        String productName = "ZARA COAT 3";
+        ProductCatalogue productCatalogue = landingPage.loginApplication("keshav@gmail.com", "Iamking@000");
+        List<WebElement> products = productCatalogue.getProductList();
+        productCatalogue.addProductToCart(productName);
+        CartPage cartPage = productCatalogue.goToCartPage();
+        Boolean match = cartPage.VerifyProductDisplay("ZARA COAT 33");
+        Assert.assertFalse(match);
+
+
+    }
+
+
+}
