5901fd292472f2f9321e67ffda89e01b4277ab1a
keshav.jha <keshav.jha@medibuddy.in>
2022-09-21 08:25:38 +0530

    Added Retry

---
+++ src/test/java/academy/testcomponents/Retry.java
@@ -0,0 +1,21 @@
+package academy.testcomponents;
+
+import org.testng.IRetryAnalyzer;
+import org.testng.ITestResult;
+
+public class Retry implements IRetryAnalyzer {
+
+    int count = 0;
+    int maxTry = 1;
+
+    @Override
+    public boolean retry(ITestResult result) {
+        if (count < maxTry) {
+            count++;
+            return true;
+        }
+        return false;
+    }
+
+
+}
